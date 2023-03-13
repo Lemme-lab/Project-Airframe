bcc22034b994b5725858d445fe5547bc39ca5945
lemme <Lucas.lenarcic@gmail.com>
2023-03-13 16:34:47 +0100

    currently working on Bluetooth

--- Code/airframe_smartwatch_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Code/airframe_smartwatch_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_blue
 import location
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterBluePlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
 }
